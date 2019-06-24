9d7e30b0ad6a1ab9ece48f43ce1fa68365978f9f
ChongyouXu <32831575+ChongyouXu@users.noreply.github.com>
2019-06-24 21:19:45 +0800

    test

---
+++ test/scripts/main.js
@@ -0,0 +1,42 @@
+let myImage = document.querySelector('img');
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/ico13.png')
+    }
+    else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+
+}
+function setHeading(name) {
+    let myHeading = document.querySelector('h1');
+    myHeading.textContent = 'Mozilla 酷毙了,' + name + '!';
+
+}
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+      localStorage.setItem('name', myName);
+        setHeading(myName);
+    //if(myName!=null&&myName!==''){
+    //  localStorage.setItem('name', myName);
+     //   setHeading(myName);
+   // }
+  //  else{
+     //   alert("请输入正确的名字!");
+     //   setUserName();
+   // }
+   
+    
+
+}
+let storedName = localStorage.getItem('name');
+if (!storedName) {
+    setUserName();
+}
+else {
+    setHeading(storedName);
+}
+
+let myButton = document.querySelector('button');
+myButton.onclick = setUserName;
