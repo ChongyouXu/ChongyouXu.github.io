312e46422fc5829befcaa9197a7ae899a0512d85
ChongyouXu <32831575+ChongyouXu@users.noreply.github.com>
2019-06-26 22:32:57 +0800

    访问次数

--- Glory-stone/scripts/main.js
+++ Glory-stone/scripts/main.js
@@ -1,1 +1,8 @@
-
+if (localStorage.pagecount)
+  {
+    localStorage.pagecount=Number(localStorage.pagecount)+1; 
+  }
+else{
+       localStorage.pagecount=1;
+    }
+document.getElementById("visit-counts").innerHTML="访问次数："+localStorage.pagecount+"次"
